86444c45c7c97202121f9ff39ccafb2c186530b0
rabbyananto <rabby.ananto@gmail.com>
2024-04-21 14:41:00 +0200

    update header

--- todo/src/Header.jsx
+++ todo/src/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Hearder() {
+function Header() {
   return (
     <header>
       <h1>Keeper</h1>
@@ -8,4 +8,4 @@ function Hearder() {
   );
 }
 
-export default Hearder;
+export default Header;
